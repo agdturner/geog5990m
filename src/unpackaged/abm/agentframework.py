0f78e74083fbe611ae1242974ce8db7edc178f88
Andy Turner <Andy Turner>
2018-10-29 01:04:18 +0000

    Updated model6.py to the end of the I/O practical.

--- src/unpackaged/abm/agentframework.py
+++ src/unpackaged/abm/agentframework.py
@@ -1,17 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 23 12:24:16 2018
-
-@author: geoagdt
-"""
 import random
 
 class Agent():
     
+    #def __init__(self, environment):
+    def __init__(self, environment):
+        self.environment = environment
+        self.store = 0
+        # Find out the size of environment inside the agents
+        self.width = len(environment);
+        self.height = len(environment[0])
+        self._x = random.randint(0,self.width)
+        self._y = random.randint(0,self.height)
+        #self._x = random.randint(0,99)
+        #self._y = random.randint(0,99)
+    
+    '''
     def __init__ (self):
         pass
         self._x = random.randint(0,99)
         self._y = random.randint(0,99)
+    '''
     
     '''    
     def __init__ (self, x, y):
@@ -22,13 +30,13 @@ class Agent():
     
     def move(self):
         if random.random() < 0.5:
-            self._x = (self._x + 1) % 100
+            self._x = (self._x + 1) % self.width
         else:
-            self._x = (self._x - 1) % 100
+            self._x = (self._x - 1) % self.width
         if random.random() < 0.5:
-            self._y = (self._y + 1) % 100
+            self._y = (self._y + 1) % self.height
         else:
-            self._y = (self._y - 1) % 100
+            self._y = (self._y - 1) % self.height
     
     @property
     def x(self):
@@ -54,3 +62,27 @@ class Agent():
 
     def hi(self):
         print("Hello world I am at x =", self._x, "y =", self._y)
+        
+    def __str__(self):
+        return "Location x = " + str(self._x) + ", y = " + str(self._y) + ", store = " + str(self.store)
+        
+    def eat(self):
+        # If more than 10 get 10
+        if self.environment[self._y][self._x] > 10:
+            self.environment[self._y][self._x] -= 10
+            self.store += 10
+        else:
+            # Store what is left
+            self.store += self.environment[self._y][self._x]
+            self.environment[self._y][self._x] = 0
+        #print(str(self.store))
+        if self.store > 100:
+            self.environment[self._y][self._x] = self.environment[self._y][self._x] + 100
+            print(str(self))
+            self.store = 0
+            print(str(self))
+            #self.__str__.__call__
+            #print("Location x =", self._x, "y =", self._y, "store =", str(self.store))
+        
+        
+    
--- src/unpackaged/abm/model6.py
+++ src/unpackaged/abm/model6.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 28 22:07:41 2018
-
-@author: geoagdt
-"""
 import csv
 import matplotlib
 import agentframework
@@ -13,53 +7,103 @@ import matplotlib.pyplot
 
 environment = []
  
-#f = open('data.csv', newline='') 
+# read csv into environment
+'''
 f = open('in.txt', newline='') 
 reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
 for row in reader: # A list of rows
     rowlist = []
-    for value in row: # A list of value
+    for value in row: # A list of values
         rowlist.append(value)
         #print(value)
     environment.append(rowlist)
 f.close()
+'''
+# The following way is better as:
+# The 'with' keyword sets up a Context Manager, which temporarily deals with 
+# how the code runs. This closes the file automatically when the clause is 
+# left. 
+with open('in.txt', newline='') as f:
+    reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader:
+        rowlist = []
+        for value in row:
+            rowlist.append(value)
+            #print(value)
+        environment.append(rowlist)
 
-a = agentframework.Agent()
-print(type(a)) #  Print the type of aTo check the class:
-print(isinstance(a, agentframework.Agent)) # Check if a is an agentframework.Agent type
-print(a)
-print(a.x)
-print(a.y)
-a.hi()
-a.move()
-a.hi()
-
+'''
 def distance_between(agents_row_a, agents_row_b):
-     return (((agents_row_a.x - agents_row_b.x)**2) + 
-     ((agents_row_a.y - agents_row_b.y)**2))**0.5
+     return (((agents_row_a.getx() - agents_row_b.getx()))**2) + 
+     ((agents_row_a.gety() - agents_row_b.gety())**2))**0.5
+'''
+'''
+def distance_between(agents_row_a, agents_row_b):
+     return (((agents_row_a.x() - agents_row_b.x()))**2) + 
+     ((agents_row_a.y() - agents_row_b.y())**2))**0.5
+'''
 
 num_of_agents = 10
-num_of_iterations = 100
+num_of_iterations = 1000
 agents = []
 
 # Make the agents.
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent())
+    agents.append(agentframework.Agent(environment))
 
 # Move the agents.
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
         agents[i].move()
- 
-matplotlib.pyplot.imshow(environment)
+        agents[i].eat()
+
 #matplotlib.pyplot.xlim(0, 99)
 #matplotlib.pyplot.ylim(0, 99)
-
+matplotlib.pyplot.xlim(0, len(environment))
+matplotlib.pyplot.ylim(0, len(environment[0]))
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i].y,agents[i].x)
+    matplotlib.pyplot.scatter(agents[i].getx(),agents[i].gety())
 matplotlib.pyplot.show()
 
+'''
 for agents_row_a in agents:
      for agents_row_b in agents:
          distance = distance_between(agents_row_a, agents_row_b) 
+'''
+        
+# Write out the environment as a file
+'''
+f2 = open('dataout.csv', 'w', newline='') 
+writer = csv.writer(f2, delimiter=' ')
+for row in environment: 
+    writer.writerow(row) # List of values.
+f2.close() 
+'''
+with open('dataout.csv', 'w', newline='') as f2:
+    writer = csv.writer(f2, delimiter=' ')
+    for row in environment:
+        writer.writerow(row)
+        '''
+        for value in row:
+            print(value)
+            #writer.write(value)
+        '''
+        
+# Calculate total amount stored by all the agents
+total = 0
+for a in agents:
+    total += a.store
+    #print(total)
 
+# Append total to dataout2.txt
+with open("dataout2.txt", "a") as f3:
+    f3.write(str(total) + "\n")
+    #f3.write("\n")
+    f3.flush  
+f3.close
+
+# Can you get the agents to sick up their store in a location if 
+# they've been greedy guts and eaten more than 100 units? 
+# (note that when you add or subtract from the map, the colours will 
+# re-scale).
