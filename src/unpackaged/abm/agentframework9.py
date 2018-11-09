47aec979435e6d57c54723afec4ae5d9f20ade17
nickgouldleeds <44134361+nickgouldleeds@users.noreply.github.com>
2018-11-09 12:15:06 +0000

    Improved documentation and workflow.

--- src/unpackaged/abm/agentframework9.py
+++ src/unpackaged/abm/agentframework9.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 __version__  1.0.0
+
+Holds a basic Agent class. Agents have a location in a 2D plane represented by 
+a raster environment which is shared as is a list of all other agents with all 
+other agents in the environment.
 """
 import random
 
@@ -9,6 +13,17 @@ class Agent():
     
     #def __init__(self, environment):
     def __init__(self, environment, agents, random_seed, x, y):
+        """
+        Initialises an agent.
+        
+        Postional arguments:
+        environment -- the raster environment to be shared with all agents.
+        agents -- a reference to all agents in environmet
+        random_seed -- a number used to initialise the random seed for this 
+        agent
+        x -- the x axis locational coordinate for this agent.
+        y -- the y axis locational coordinate for this agent.
+        """
         self.environment = environment
         self.agents = agents
         self.store = 0
@@ -34,14 +49,20 @@ class Agent():
         #print("y", self._y)
       
     def move(self):
-        if random.random() < 0.5:
-            self._x = (self._x + 1) % self.width
-        else:
-            self._x = (self._x - 1) % self.width
-        if random.random() < 0.5:
-            self._y = (self._y + 1) % self.height
-        else:
-            self._y = (self._y - 1) % self.height
+        """
+        Moves an agent though there is a 1 in 3 chance of not moving in either 
+        the x or y direction.
+        """
+        if random.random() >= 1/float(3):
+            if random.random() < 0.5:
+                self._x = (self._x + 1) % self.width
+            else:
+                self._x = (self._x - 1) % self.width
+        if random.random() >= 1/float(3):
+            if random.random() < 0.5:
+                self._y = (self._y + 1) % self.height
+            else:
+                self._y = (self._y - 1) % self.height
     
     @property
     def x(self):
@@ -66,12 +87,24 @@ class Agent():
         self._y = value
 
     def hi(self):
-        print("Hello world I am at x =", self._x, "y =", self._y)
+        """
+        Prints the Agent location of an agent to std.out
+        """
+        #print("Agent location: x =", self._x, "y =", self._y)
+        print(self)
         
     def __str__(self):
+        """
+        Returns:
+        A description of the agent detailing it's location and store.
+        """
         return "Location x = " + str(self._x) + ", y = " + str(self._y) + ", store = " + str(self.store)
         
     def eat(self):
+        """
+        Agent eats/removes some of environemnt. If their store gets to be 
+        greater than 100, then 50% of it is dumped into the environment.
+        """
         # If more than 10 get 10
         if self.environment[self._y][self._x] > 10:
             self.environment[self._y][self._x] -= 10
@@ -82,14 +115,20 @@ class Agent():
             self.environment[self._y][self._x] = 0
         #print(str(self.store))
         if self.store > 100:
-            self.environment[self._y][self._x] = self.environment[self._y][self._x] + 100
+            self.environment[self._y][self._x] = self.environment[self._y][self._x] + 50
             #print(str(self))
-            self.store = 0
+            self.store = 50
             #print(str(self))
             #self.__str__.__call__
             #print("Location x =", self._x, "y =", self._y, "store =", str(self.store))
             
     def share_with_neighbours(self, neighbourhood):
+        """
+        Agent shares some of store with neighbours.
+        
+        Postional arguments:
+        neighbourhood -- the distance within which agents share an instance of this Agent class
+        """
         #print(neighbourhood)
         for agent in self.agents:
             # Don't share with self for speed (not that it matters much)
@@ -103,6 +142,15 @@ class Agent():
                     #print("sharing " + str(dist) + " " + str(ave))
  
     def distance_between(self, agent):
+        """
+        Calculate and return the distance between self and agent.
+        
+        Postional arguments:
+        agent -- an instance of this Agent class
+        
+        Returns:
+        The distance between self and agent.
+        """
         return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5 
    
         
--- src/unpackaged/abm/model9.py
+++ src/unpackaged/abm/model9.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 __version__  0.1.0
-As with model8.py I am wanting to get the agents wandering about on the 
-Environment, but this needs more work...
+
+This model is run from a GUI in which there is a run menugenerates a number of agents and 
 """
 import agentframework9 as af
 import random
@@ -23,46 +23,49 @@ import bs4
 #from bs4 import BeautifulSoup as soup
 #import sys
 
-'''
+"""
 Step 1: Initialise parameters
-'''
+"""
 print("Step 1: Initialise parameters")
 print("argv", argv)
-if len(argv) < 5:
+if len(argv) < 6:
     num_of_agents = 10
-    num_of_iterations = 1000
+    num_of_iterations = 100
     neighbourhood = 20
     random_seed = 0
+    agent_store = 90
     print("argv does not contain the expected number of arguments")
     print("len(argv)", len(argv))
     print("expected len(argv) 5")
     print("expecting:")
     print("argv[1] as a integer number for num_of_agents")
-    print("argv[1] as a integer number for num_of_iterations")
-    print("argv[1] as a integer number for neighbourhood")
-    print("argv[1] as a integer number for random_seed for setting the random seed")
+    print("argv[2] as a integer number for num_of_iterations")
+    print("argv[3] as a integer number for neighbourhood")
+    print("argv[4] as a integer number for random_seed for setting the random seed")
+    print("argv[5] as a integer number for between 0 and 100. This is the level that if all agents stores reach, the program ends.")
 else:
     # set parameters from argv
     num_of_agents = int(argv[1])
     num_of_iterations = int(argv[2])
     neighbourhood = int(argv[3])
     random_seed = int(argv[4])
+    agent_store = int(argv[5])
 print("num_of_agents", str(num_of_agents))
 print("num_of_iterations", str(num_of_iterations))
 print("neighbourhood", str(neighbourhood))
 print("random_seed", str(random_seed))
 random.seed(random_seed)
 
-'''
+"""
 Step 2: Initialise GUI main window.
-'''
+"""
 print("Step 2: Initialise GUI main window")
 root = tkinter.Tk() # Main window.
 root.wm_title("Model")
 
-'''
+"""
 Step 3: Get data from the web.
-'''
+"""
 print("Step 3: Get data from the web.")
 url = 'https://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html'
 r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
@@ -73,14 +76,14 @@ td_ys = soup.find_all(attrs={"class" : "y"})
 td_zs = soup.find_all(attrs={"class" : "z"})
 # Note that there is a new range in the data read in and agents are only 
 # initialise in the first 100 by 100 npart of enviroement
-''' print test
+""" print test
 #print(td_xs) 
 for td in td_xs:
     print(td.text)
 #print(td_ys)
 #print(td_zs)
-'''
-''' Code exploration
+"""
+""" Code exploration
 #response = urllib.request.urlopen(url)
 #response = urllib.request.urlopen(url, cafile=certifi.where())
 #response = urllib.request.urlopen(url, data=None, cafile=None, capath=None, cadefault=False, context=None)
@@ -98,12 +101,12 @@ tds = tr.find_all("td")
 for td in tds:
     print (td.text) 
 #All tags are lowercased during search
-''' 
+""" 
 
-'''
+"""
 Step 4: Initialise environment this will contain data about the spatial 
 environment in which agents act.
-'''
+"""
 print("Step 4: Initialise environment this will contain data about the",
       "spatial environment in which agents act.")
 environment = []
@@ -117,9 +120,9 @@ with open('in.txt', newline='') as f:
             #print(value)
         environment.append(rowlist)
 
-'''
+"""
 Step 5: Initialise agents.
-'''
+"""
 print("Step 5: Initialise agents.")
 agents = []
 # Make the agents.
@@ -130,23 +133,51 @@ for i in range(num_of_agents):
     y = int(td_ys[i].text)
     x = int(td_xs[i].text)
     # Add 1 to random seed to get each agent initialised and moving differently
-    random_seed += 1
+    random_seed += 1000
     agents.append(af.Agent(environment, agents, random_seed, y, x))
 
-'''
-Step 4: Animate acting agents.
-'''
-print("Step 4: Animate acting agents.")
+"""
+Step 6: Initialise the GUI
+"""
+print("Step 6: Initialising GUI.")
+# Set up the figure and loop variables.
 fig = pyplot.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
-
 carry_on = True
+init = True
+halted = False
+rerunid = 0
+total_ite = 0;
+print("A GUI window should appear. Please select \"Run Model\" from the \"Model\" menu to run the model.")
 
+"""
+Step 7: Animate acting agents.
+"""
 def update(frame_number):
-    
-    global carry_on  #Not actually needed as we're not assigning, but clearer
+    """
+    Updates the display in the animation.
+    """
+    global carry_on
+    global init
+    global halted
+    global rerunid
+    if (init == True):
+        print("Step 7: Animate acting agents.")
+        print("Start .", end='')
+        init = False
+    else:
+        if (halted):
+            rerunid += 1
+            print("Continuing", rerunid, end='')
+            halted = False
+        else:
+            print(" .", end='')
+        
     fig.clear()
-    
+    """
+    Clear the figure
+    """
+   
     # Process the agents in a randomish order.
     if (carry_on):
         # Shuffle agents
@@ -155,15 +186,14 @@ def update(frame_number):
             agents[i].move()
             agents[i].eat()
             agents[i].share_with_neighbours(neighbourhood)
-        # Stop if all agents have more than 50 store
+        # Stop if all agents have more than agent_store store
+        half_full_agent_count = 0
         for i in range(num_of_agents):
-            half_full_agent_count = 0
-            if (agents[i].store > 30):
+            if (agents[i].store > agent_store):
                 half_full_agent_count += 1
         if (half_full_agent_count == num_of_agents):
             carry_on = False
-            print("stopping condition")
-
+            print(" all agent stores are greater than", agent_store, "run ended after ", end='')
     # Plot            
     # Plot environment
     pyplot.xlim(0, len(environment))
@@ -175,39 +205,62 @@ def update(frame_number):
         #print(agents[i].getx(),agents[i].gety())
     
 def gen_function(b = [0]):
+    """
+    A halting function for the animation.
+    """
     a = 0
-    global carry_on #Not actually needed as we're not assigning, but clearer
+    global carry_on
+    global halted
+    global total_ite
     while  (a < num_of_iterations) & (carry_on): 
         yield a			#: Returns control and waits next call.
         a = a + 1
-             
+        total_ite += 1
+    halted = True
+    if (a >= num_of_iterations):
+        print(" run stopped after", num_of_iterations, "iterations.")
+    else:
+        print(total_ite, "iterations.")
+        exiting()
+    
 #animation = anim.FuncAnimation(fig, update, interval=1)
 #animation = anim.FuncAnimation(fig, update, interval=1, repeat=False, frames=10)
 #animation = anim.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations)
 animation = anim.FuncAnimation(fig, update, frames=gen_function, repeat=False)
-"""Create animated plot. Continues to update the plot until stopping criteria is met.""" 
+"""
+Create animated plot. Continues to update the plot until stopping criteria is 
+met.
+"""
 
 #pyplot.show()
-"""Display the plot."""
-
+#"""Display the plot."""
 def run():
     global animation
     animation = anim.FuncAnimation(fig, update, frames=gen_function, repeat=False)
-    canvas.show()
+    #canvas.show()
+    canvas.draw()
 
 canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
-canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1) 
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
 menu_bar = tkinter.Menu(root)
 root.config(menu=menu_bar)
 model_menu = tkinter.Menu(menu_bar)
 menu_bar.add_cascade(label="Model", menu=model_menu)
 model_menu.add_command(label="Run model", command=run)
-model_menu.add_command(label="Exit", command=root.quit) 
-model_menu.add_command(label="Destroy", command=root.destroy)
+#model_menu.add_command(label="Exit", command=root.quit)
+#model_menu.add_command(label="Destroy", command=root.destroy)
+"""
+Since overriding the WM_DELETE_WINDOW protocol the preceding (now commented) 
+two commented lines of code are no longer needed.
+"""
 
 # The process is quit as well as destroying the main window (root) on exit
 def exiting():
+    """
+    Exits the program and kills the GUI window if the GUI window is closed.
+    """
+    print("Step 8: End Program.")
     #print("we're here")
     root.quit()
     root.destroy()
@@ -216,3 +269,5 @@ def exiting():
 root.protocol('WM_DELETE_WINDOW', exiting) 
 
 tkinter.mainloop() # Wait for interactions.
+
+print("Thank you for running the model.")
